5970c0673e134e97bee19ae60dd17d2e728ed1cb
Sharma Muridzo <smuridzo20@gmail.com>
2023-01-25 17:04:19 +0100

    Update on PasswordViewModel

--- Fifa Wars/ViewModels/PasswordViewModel.swift
+++ Fifa Wars/ViewModels/PasswordViewModel.swift
@@ -11,12 +11,17 @@ import SwiftUI
 @MainActor
 
 
+
+
 class PasswordViewModel: ObservableObject {
+    @Binding var emails: [String]
     @Published var email: String = ""
     @Published var showingPasswordScreen: Bool = false
     @Published var alertMessage: String = ""
     @Published var denyMessage: String = ""
     @Published var showAlert: Bool = false
+    
+    
     
     init(email: String, showingPasswordScreen: Bool, alertMessage: String, denyMessage: String, showAlert: Bool) {
         self.email = email
@@ -30,17 +35,17 @@ class PasswordViewModel: ObservableObject {
         self.email = ""
     }
     
-//    func authenticateEmail(email: String) {
-//        @StateObject var register = RegisterViewModel()
-//        var emails: [String] = [$register.emails]
-//
-////        if email.contains(emails) {
-////            showAlert.toggle()
-////            alertMessage = "Username exists"
-////        } else {
-////            print("Email is not in database.")
-////        }
-//    }
+    func authenticateEmail(email: String) {
+        //@StateObject var register = RegisterViewModel()
+        @Binding var emails: [String]
+
+        if self.email {
+            showAlert.toggle()
+            alertMessage = "Username exists"
+        } else {
+            print("Email is not in database.")
+        }
+    }
     
     
 }
